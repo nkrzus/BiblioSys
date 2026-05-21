8ba867a258a6fd4a17698add60be1f5aea0fba5a
Natalia Krzus <nkrzus95@gmail.com>
2026-05-21 23:39:40 +0200

    Modified Adding User View, missing passwd

--- BiblioSys/Models/User.cs
+++ BiblioSys/Models/User.cs
@@ -16,6 +16,8 @@ namespace Bibliosys.Model
 
         public string Email { get; set; }
 
+        public string Password { get; set; }
+
         public double AccountBalance { get; set; } = 0;
 
         public List<Reservation> ?Reservations { get; set; }
